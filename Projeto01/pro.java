65f69a690afa7c7fd6c338c469496005a0386bd5
Gabriel Vilhena <92929267+GabrielMonte@users.noreply.github.com>
2021-11-11 15:19:27 -0300

    atua

---
+++ Projeto01/pro.java
@@ -0,0 +1,7 @@
+public class pro {
+    
+    public static void main(String[] args) { //o projeto começar quando o programa encontrar esse "public...chamado "ponto de entrada"
+        System.out.printf("%nOlá Mundo, eu sou o primeiro programa"); //serve para mostrar o programa sendo executado.
+        
+    }
+}
